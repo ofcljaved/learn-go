19a05e98439e7f9e23822e8814db07ffde986261
ofcljaved <javedofficialme@gmail.com>
2024-12-17 18:17:10 +0530

    learn: variable

--- learn/main.go
+++ learn/main.go
@@ -2,39 +2,4 @@ package main
 
 import "fmt"
 
-func main() {
-    /*
-    **
-    ** This is to check mutiple primitve type in go
-    **
-    */
-
-    // fmt.Println("go" + "lang")
-    //
-    // fmt.Println("1+1 = ",      1+1)
-    // fmt.Println("7.0/3.0 = ", 7.0/3.0)
-    //
-    // fmt.Println(true && false)
-    // fmt.Println(true || false)
-    // fmt.Println(!true)
-
-    /*
-    **
-    ** This is to initialise variable
-    **
-    */
-
-    // a := "apple"
-    // fmt.Println(a)
-    //
-    // var b, c int = 1,2 
-    // fmt.Println(b)
-    // fmt.Println(c)
-    //
-    // var e int
-    // fmt.Println(e)
-    //
-    // var f string
-    // fmt.Println(len(f))
-
-}
+func main() { }
---
+++ learn/var.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+    /*
+    **
+    ** This is to initialise variable
+    **
+    */
+
+    a := "apple"
+    fmt.Println(a)
+
+    var b, c int = 1,2 
+    fmt.Println(b)
+    fmt.Println(c)
+
+    var e int
+    fmt.Println(e)
+
+    var f string
+    fmt.Println(len(f))
+
+}
