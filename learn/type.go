949d2a246d90c94209297411db355844200a0767
ofcljaved <javedofficialme@gmail.com>
2024-12-17 18:16:50 +0530

    learn: primitive type

---
+++ learn/type.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    /*
+    **
+    ** This is to check mutiple primitve type in go
+    **
+    */
+
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 = ",      1+1)
+    fmt.Println("7.0/3.0 = ", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
