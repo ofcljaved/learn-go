0997d322b4b2824c897a464f9de6f19983b3aa11
ofcljaved <javedofficialme@gmail.com>
2024-12-22 08:16:51 +0530

    chore: multiple greetings via map

--- hello/hello.go
+++ hello/hello.go
@@ -11,10 +11,16 @@ func main() {
     log.SetPrefix("greetins: ")
     log.SetFlags(0)
 
-    message, err:= greetings.Hello("")
+    names :=[]string{"Jack", "Javed", "Jia"}
+
+    messages, err:= greetings.Hellos(names)
+
+    message, err:= greetings.Hello("Jack")
     if err != nil {
         log.Fatal(err)
     }
 
     fmt.Println(message)
+    fmt.Println("\r\nMultiple greetings on the way!!!\r\n")
+    fmt.Println(messages)
 }
